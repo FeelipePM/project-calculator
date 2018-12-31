a9fb4fde287deb5e769b10c0c7eb65d37b58576c
Felipe <Feelipe@email.com>
2018-12-31 11:02:10 -0200

    picking up the buttons

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -7,6 +7,7 @@ class CalcController {
 
     this._currentDate;
     this.initialize();
+    this.initButtonsEvents();
   }
 
   initialize() {
@@ -14,6 +15,25 @@ class CalcController {
     setInterval(() => {
       this.setDisplayDateTime();
     }, 1000);
+  }
+
+  addEventListenerAll(element, events, fn) {
+    events.split(" ").forEach(event => {
+      element.addEventListener(event, fn, false);
+    });
+  }
+
+  initButtonsEvents() {
+    let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+    buttons.forEach((btn, index) => {
+      this.addEventListenerAll(btn, "click drag", e => {
+        console.log(btn.className.baseVal.replace("btn-", ""));
+      });
+      this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+        btn.style.cursor = "pointer";
+      });
+    });
   }
 
   setDisplayDateTime() {

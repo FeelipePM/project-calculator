dcadf72319288f6d5379097b021275d6ffdba699
Felipe <Feelipe@email.com>
2019-01-14 16:00:26 -0200

    Display funcionando, como soma, diminuir, dividir, multiplicar

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -56,9 +56,19 @@ class CalcController {
     let last = this._operation.pop();
     let result = eval(this._operation.join(""));
     this._operation = [result, last];
+    this.setNumberToDisplay();
   }
 
-  setNumberToDisplay() {}
+  setNumberToDisplay() {
+    let lastNumber;
+    for (let i = this._operation.length - 1; i >= 0; i--) {
+      if (!this.isOperator(this._operation[i])) {
+        lastNumber = this._operation[i];
+        break;
+      }
+    }
+    this.displayCalc = lastNumber;
+  }
 
   addOperation(value) {
     if (isNaN(this.getLastOperation())) {
@@ -68,6 +78,7 @@ class CalcController {
         console.log(value);
       } else {
         this.pushOperatorion(value);
+        this.setNumberToDisplay();
       }
     } else {
       if (this.isOperator(value)) {

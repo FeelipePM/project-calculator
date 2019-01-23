e8852062fe9e65af4aaec831ec55771ae020717b
Felipe <Feelipe@email.com>
2019-01-23 12:12:35 -0200

    Corrigido bug no display da calculadora

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -1,5 +1,7 @@
 class CalcController {
   constructor() {
+    this._audio = new Audio("click.mp3");
+    this._audioOnOff = false;
     this._lastOperator = "";
     this._lastNumber = "";
 
@@ -46,10 +48,29 @@ class CalcController {
 
     this.setDisplayDateTime();
     this.pasteFromClipboard();
+
+    document.querySelectorAll(".btn-ac").forEach(btn => {
+      btn.addEventListener("dblclick", event => {
+        this.toggleAudio();
+      });
+    });
+  }
+
+  toggleAudio() {
+    this._audioOnOff = !this._audioOnOff;
+  }
+
+  playAudio() {
+    if (this._audioOnOff) {
+      this._audio.currentTime = 0;
+      this._audio.play();
+    }
   }
 
   initKeyboard() {
     document.addEventListener("keyup", event => {
+      this.playAudio();
+
       switch (event.key) {
         case "Escape":
           this.clearAll();
@@ -231,6 +252,8 @@ class CalcController {
   }
 
   execBtn(value) {
+    this.playAudio();
+
     switch (value) {
       case "ac":
         this.clearAll();
@@ -327,6 +350,10 @@ class CalcController {
   }
 
   set displayCalc(value) {
+    if (value.toString().length > 10) {
+      this.setError();
+      return false;
+    }
     this._displayCalcEl.innerHTML = value;
   }
 
